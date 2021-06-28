4abd6be97223aaa340d4a8a3e54ce0f7db30c89d
duxingxia081 <332931496@qq.com>
2021-06-28 16:50:03 +0800

    刚搭建的activiti

---
+++ src/test/java/com/jf/springbootactiviti/SpringBootActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jf.springbootactiviti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
